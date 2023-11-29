2ba3fedb547994bb2e867f53d73a63fd403172f8
boskin28 <150635516+boskin28@users.noreply.github.com>
2023-11-28 21:21:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,50 @@ import pinecone
 import streamlit as st
 import time
 
+# Authentication:
+def check_password():
+    """Returns `True` if the user had a correct password."""
+
+    def login_form():
+        """Form with widgets to collect user information"""
+        with st.form("Credentials"):
+            st.text_input("Username", key="username")
+            st.text_input("Password", type="password", key="password")
+            st.form_submit_button("Log in", on_click=password_entered)
+
+
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["username"] in st.secrets[
+            "passwords"
+        ] and hmac.compare_digest(
+            st.session_state["password"],
+            st.secrets.passwords[st.session_state["username"]],
+        ):
+            st.session_state["password_correct"] = True
+            
+            # Don't store the username or password
+            del st.session_state["password"]
+            del st.session_state["username"]
+        else:
+            st.session_state["password_correct"] = False
+
+
+    # Return True if the username + password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show inputs for username + password.
+    login_form()
+    if "password_correct" in st.session_state:
+        st.error("Username or password incorrect")
+    return False
+
+# Do not continue until authenticated
+if not check_password():
+    st.stop()
+
 
 # Create an OpenAI embeddings instance
 OPENAI_API_KEY = st.secrets['OPENAI_API_KEY']
