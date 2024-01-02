b36fa3fe69eef1b372acc06ff4b0adc0e5e9ea51
boskin28 <150635516+boskin28@users.noreply.github.com>
2024-01-02 17:05:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import pinecone
 import streamlit as st
 import time
 import hmac
+import re
 
 
 # Authentication:
@@ -53,6 +54,9 @@ if not check_password():
     st.stop()
 
 
+
+# BEGIN WEBPAGE:
+
 # Create an OpenAI embeddings instance
 OPENAI_API_KEY = st.secrets['OPENAI_API_KEY']
 embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
@@ -61,24 +65,22 @@ embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
 PINECONE_API_KEY = st.secrets['PINECONE_API_KEY']
 PINECONE_API_ENV = "us-east4-gcp"
 index_name = "bosco3000"
-index = pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_API_ENV)
 
+index = pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_API_ENV)
 docsearch = Pinecone.from_existing_index(index_name, embeddings)
 
 # Create an OpenAI LLM (Language Model) instance
-llm = ChatOpenAI(temperature=0.5, openai_api_key=OPENAI_API_KEY, model='gpt-4')
+llm = ChatOpenAI(temperature=0.3, openai_api_key=OPENAI_API_KEY, model='gpt-4')
 chain = load_qa_chain(llm, chain_type="stuff")
+
 
 # Create Question and Answer function
 def QnA(question):
-    query = question
-    docs = docsearch.similarity_search(query)
-    answer = chain.run(input_documents=docs, question=query)
+    """Returns the answer and source documents to the input question"""
+    docs = docsearch.similarity_search(question)
+    answer = chain.run(input_documents=docs, question=question)
     return answer, docs
 
-
-
-# BEGIN WEBPAGE:
 
 # Title
 st.title("BOSCO 3000")
@@ -89,34 +91,47 @@ if "messages" not in st.session_state:
 
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
+    with st.chat_message(message["role"], avatar=message["avatar"]):
         st.markdown(message["content"])
+
+user_image = '🕵️'
+assistant_image = '🤖'
 
 # Accept user input
 if prompt := st.chat_input("Ask me anything"):
     # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.session_state.messages.append({"role": "user", "avatar": user_image, "content": prompt})
     # Display user message in chat message container
-    with st.chat_message("user"):
+    with st.chat_message("user", avatar=user_image):
         st.markdown(prompt)
 
-    answer = QnA(prompt)[0]
-    docs = QnA(prompt)[1][0].page_content
+    # Retrieve a response
+    response = QnA(prompt)
+    answer = response[0]
+    docs = response[1]
+
+    # Create list of filenames
+    files = []
+    for document in docs:
+        filename = document.metadata["filename"]
+        if filename not in files:
+            files.append(filename)
 
     # Display assistant response in chat message container
-    with st.chat_message("assistant"):
+    with st.chat_message("assistant", avatar=assistant_image):
         message_placeholder = st.empty()
+        filenames_placeholder = st.empty()
         full_response = ""
-        assistant_response = answer
+
+        assistant_response = answer + '  \n\n **Files:**  \n' + '  \n'.join(files)
 
         # Simulate stream of response with milliseconds delay
-        for chunk in assistant_response.split():
+        for chunk in re.split(r"(\s+)", assistant_response):
             full_response += chunk + " "
-            time.sleep(0.05)
+            time.sleep(0.02)
             # Add a blinking cursor to simulate typing
             message_placeholder.markdown(full_response + "▌")
         message_placeholder.markdown(full_response)
 
     # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": full_response})
-
+    st.session_state.messages.append({"role": "assistant", "avatar": assistant_image, "content": full_response})
